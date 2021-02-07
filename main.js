09fdda147806accd7febc506ba182b1ec9fef6be
Asante Wiebers <Asantewiebers@gmail.com>
2021-02-07 22:55:43 +0100

    js-updated

--- main.js
+++ main.js
@@ -10,11 +10,12 @@ reset: true
 // home section/
 ScrollReveal().reveal('.banner',{delay:200});
 ScrollReveal().reveal('.Logo-header',{delay:500});
+ScrollReveal().reveal('.navigation',{delay:500});
 
 // Gallery 
 
-ScrollReveal().reveal('#foto-section',{delay:500});
-ScrollReveal().reveal('#footer',{delay:500});
+ScrollReveal().reveal('#foto-section',{interval:200});
+ScrollReveal().reveal('#footer',{delay:400});
 
 
 
