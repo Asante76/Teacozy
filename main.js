bf2afa710f316e18190e41fc030256a10fb99f3a
Asante Wiebers <Asantewiebers@gmail.com>
2021-02-07 20:14:42 +0100

    Js added

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-ScrollReveal().reveal
+const sr = ScrollReveal ({
+
+origin: 'top',
+distance: '80px',
+duration: 2000,
+reset: true
+
+
+});
+// home section/
+ScrollReveal().reveal('.banner',{delay:200});
+ScrollReveal().reveal('.Logo-header',{delay:500});
+
+// Gallery 
+
+ScrollReveal().reveal('#foto-section',{delay:500});
+ScrollReveal().reveal('#footer',{delay:500});
+
+
+
+
+
